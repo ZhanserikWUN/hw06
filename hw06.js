0fe0f58c4e92a6f5bc5b9ea596752234670a4ef4
Zhanserik <mpbszhan@gmail.com>
2023-04-13 17:16:55 +0600

    first commit

---
+++ hw06.js
@@ -0,0 +1,20 @@
+// 1 task
+
+// let  removeButton = document.getElementById('removeButton');
+
+// removeButton.addEventListener('click', function() {
+//     // Get the div element by its ID
+//     let textDiv = document.getElementById('text');
+
+//     textDiv.remove();
+// });
+
+
+
+//------------------------------------------------------------------
+// task 2
+// button.addEventListener("click", () => alert("1"));
+// button.removeEventListener("click", () => alert("1"));
+// button.onclick = () => alert(2);
+
+//отобразится предупреждающее сообщение только с текстом "2", 
